ca809a167a3e4a0e6a78bb1e40f20bbeb170c67b
Juhyung Park <qkrwngud825@gmail.com>
2022-03-31 21:37:53 +0900

    lmkd: featurize loggings to kernel trace
    Change-Id: I931e7cd16021f8ba49f381b8c654bd8628ee2dfb
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- lmkd.cpp
+++ lmkd.cpp
@@ -175,6 +175,8 @@
 
 #define NATIVE_PID_FD (-128)
 
+#define ENABLE_TRACING
+
 /* default to old in-kernel interface if no memory pressure events */
 static bool use_inkernel_interface = true;
 static bool has_inkernel_module;
@@ -2720,6 +2722,7 @@ static int parse_one_zone_watermark(char *buf, struct watermark_info *w)
     return ret;
 }
 
+#ifdef ENABLE_TRACING
 static void trace_log(const char *fmt, ...)
 {
     char buf[BUF_MAX];
@@ -2758,6 +2761,9 @@ static void trace_log(const char *fmt, ...)
     ALOG##X(fmt);              \
     trace_log(fmt);            \
 })
+#else
+#define ULMK_LOG(X, fmt...) ALOG##X(fmt)
+#endif
 
 static struct proc *proc_adj_lru(int oomadj) {
   return (struct proc *)adjslot_tail(&procadjslot_list[ADJTOSLOT(oomadj)]);
