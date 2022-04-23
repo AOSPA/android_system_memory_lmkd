98d77c46e79fbb9a2a9d9596c9400726ae237c92
전윤재 <psijic213@gmail.com>
2022-04-23 14:02:00 +0000

    lmkd: Fix a comparison operation with uninitialized variable.
    Prevent comparing uninitialized poll_start_tm with curr_tm in call_handler().
    The bug caused by this has been fixed by the commit: d816ab.
    But the main bug is not fixed yet and it may cause problem
    in later if we add another operations in this if block.
    
    Change-Id: Id13318297a2cbf2f9784134a2ccd648cc221e8c4
    Signed-off-by: Yoonjae Jeon <yj213.jeon@samsung.com>

--- lmkd.cpp
+++ lmkd.cpp
@@ -4640,7 +4640,8 @@ static void call_handler(struct event_handler_info* handler_info,
         resume_polling(poll_params, curr_tm);
         break;
     case POLLING_DO_NOT_CHANGE:
-        if (get_time_diff_ms(&poll_params->poll_start_tm, &curr_tm) > psi_window_size_ms) {
+        if (poll_params->poll_handler &&
+            get_time_diff_ms(&poll_params->poll_start_tm, &curr_tm) > psi_window_size_ms) {
             /* Polled for the duration of PSI window, time to stop */
             poll_params->poll_handler = NULL;
             poll_params->paused_handler = NULL;
