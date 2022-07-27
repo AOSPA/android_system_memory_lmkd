cares-davinci/Hvalve

commit 674e711fa7ede2b1883ecaef7b5f53f9d43fee10

lmkd: Add improvements for lmkd
Add improvements to lmkd's killing strategy, such as:
considering the pgskip stats, upgrading the continuous
medium events to critical events, and killing unconditionally
when the system is in direct reclaim and processes are being
throttled.

CRs-Fixed: 2895857
Change-Id: I875f186a1a4f725be30142caad20d12847deec1c

lmkd.cpp
@@ -157,6 +157,7 @@
 /* ro.lmk.psi_complete_stall_ms property defaults */
 #define DEF_COMPLETE_STALL 70
 
+#define PSI_CONT_EVENT_THRESH (4)
 #define LMKD_REINIT_PROP "lmkd.reinit"
 
 #define PSI_OLD_LOW_THRESH_MS 70
@@ -237,6 +238,7 @@ static bool last_event_upgraded = false;
 static int count_upgraded_event;
 static long pa_update_timeout_ms = 60000; /* 1 min */
 static int kpoll_fd;
+static int psi_cont_event_thresh = PSI_CONT_EVENT_THRESH;
 /* PSI window related variables */
 static int psi_window_size_ms = PSI_WINDOW_SIZE_MS;
 static int psi_poll_period_scrit_ms = PSI_POLL_PERIOD_SHORT_MS;
@@ -252,6 +254,7 @@ static android_log_context ctx;
 enum polling_update {
     POLLING_DO_NOT_CHANGE,
     POLLING_START,
+    POLLING_CRIT_UPGRADE,
     POLLING_PAUSE,
     POLLING_RESUME,
 };
@@ -2484,6 +2487,7 @@ repeat:
                 break;
 
             procp->pid = pid;
+            procp->pidfd = -1;
             procp->uid = 0;
             procp->oomadj = oomadj;
             proc_insert(procp);
@@ -2831,13 +2835,21 @@ struct zone_watermarks {
     long min_wmark;
 };
 
+struct zone_meminfo {
+    int64_t nr_free_pages;
+    int64_t cma_free;
+    struct zone_watermarks watermarks;
+
+};
+
 /*
  * Returns lowest breached watermark or WMARK_NONE.
  */
-static enum zone_watermark get_lowest_watermark(union meminfo *mi,
-                                                struct zone_watermarks *watermarks)
+static enum zone_watermark get_lowest_watermark(union meminfo *mi __unused,
+                                                struct zone_meminfo *zmi)
 {
-    int64_t nr_free_pages = mi->field.nr_free_pages - mi->field.cma_free;
+    struct zone_watermarks *watermarks = &zmi->watermarks;
+    int64_t nr_free_pages = zmi->nr_free_pages - zmi->cma_free;
 
     if (nr_free_pages < watermarks->min_wmark) {
         return WMARK_MIN;
@@ -2882,21 +2894,32 @@ static void log_zone_watermarks(struct zoneinfo *zi,
     }
 }
 
-void calc_zone_watermarks(struct zoneinfo *zi, struct zone_watermarks *watermarks) {
-    memset(watermarks, 0, sizeof(struct zone_watermarks));
+void calc_zone_watermarks(struct zoneinfo *zi, struct zone_meminfo *zmi, int64_t *pgskip_deltas) {
+    struct zone_watermarks *watermarks;
+
+    memset(zmi, 0, sizeof(struct zone_meminfo));
+    watermarks = &zmi->watermarks;
 
     for (int node_idx = 0; node_idx < zi->node_count; node_idx++) {
         struct zoneinfo_node *node = &zi->nodes[node_idx];
+        int i = VS_PGSKIP_FIRST_ZONE;
         for (int zone_idx = 0; zone_idx < node->zone_count; zone_idx++) {
             struct zoneinfo_zone *zone = &node->zones[zone_idx];
 
+            while (pgskip_deltas[PGSKIP_IDX(i)]  < 0) ++i;
+
             if (!zone->fields.field.present) {
+                i++;
                 continue;
             }
 
-            watermarks->high_wmark += zone->max_protection + zone->fields.field.high;
-            watermarks->low_wmark += zone->max_protection + zone->fields.field.low;
-            watermarks->min_wmark += zone->max_protection + zone->fields.field.min;
+            if (!pgskip_deltas[PGSKIP_IDX(i++)]) {
+                zmi->nr_free_pages += zone->fields.field.nr_free_pages;
+                zmi->cma_free += zone->fields.field.nr_free_cma;
+                watermarks->high_wmark += zone->max_protection + zone->fields.field.high;
+                watermarks->low_wmark += zone->max_protection + zone->fields.field.low;
+                watermarks->min_wmark += zone->max_protection + zone->fields.field.min;
+            }
         }
     }
 
@@ -2926,15 +2949,16 @@ static void log_meminfo(union meminfo *mi, enum zone_watermark wmark)
     }
 }
 
-static void log_pgskip_stats(union vmstat *vs, int64_t *init_pgskip)
+static void fill_log_pgskip_stats(union vmstat *vs, int64_t *init_pgskip, int64_t *pgskip_deltas)
 {
-    int64_t pgskip_deltas[VS_PGSKIP_LAST_ZONE - VS_PGSKIP_FIRST_ZONE + 1] = {0};
     unsigned int i;
 
     for (i = VS_PGSKIP_FIRST_ZONE; i <= VS_PGSKIP_LAST_ZONE; i++) {
         if (vs->arr[i] >= 0) {
             pgskip_deltas[PGSKIP_IDX(i)] = vs->arr[i] -
                                            init_pgskip[PGSKIP_IDX(i)];
+        } else {
+            pgskip_deltas[PGSKIP_IDX(i)] = -1;
         }
     }
 
@@ -2960,19 +2984,20 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
         NO_RECLAIM = 0,
         KSWAPD_RECLAIM,
         DIRECT_RECLAIM,
+        DIRECT_RECLAIM_THROTTLE,
     };
     static int64_t init_ws_refault;
     static int64_t prev_workingset_refault;
     static int64_t base_file_lru;
     static int64_t init_pgscan_kswapd;
     static int64_t init_pgscan_direct;
+    static int64_t init_direct_throttle;
     static int64_t init_pgskip[VS_PGSKIP_LAST_ZONE - VS_PGSKIP_FIRST_ZONE + 1];
     static int64_t swap_low_threshold;
     static bool killing;
     static int thrashing_limit = thrashing_limit_pct;
-    static struct zone_watermarks watermarks;
-    static struct timespec wmark_update_tm;
     static struct wakeup_info wi;
+    static struct zone_meminfo zone_mem_info;
     static struct timespec last_pa_update_tm;
     static int64_t init_compact_stall;
     static struct timespec thrashing_reset_tm;
@@ -2996,6 +3021,8 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
     int swap_util = 0;
     long since_thrashing_reset_ms;
     int64_t workingset_refault_file;
+    int64_t pgskip_deltas[VS_PGSKIP_LAST_ZONE - VS_PGSKIP_FIRST_ZONE + 1] = {0};
+    struct zoneinfo zi;
 
     ULMK_LOG(D, "%s pressure event %s", level_name[level], events ?
              "triggered" : "polling check");
@@ -3069,7 +3096,7 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
              mi.field.free_swap, mi.field.total_swap,
              (mi.field.free_swap * 100) / (mi.field.total_swap + 1));
     }
-    log_pgskip_stats(&vs, init_pgskip);
+    fill_log_pgskip_stats(&vs, init_pgskip, pgskip_deltas);
 
     /* Check free swap levels */
     if (swap_free_low_percentage) {
@@ -3092,6 +3119,9 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
             init_pgskip[PGSKIP_IDX(i)] = vs.arr[i];
         }
         reclaim = DIRECT_RECLAIM;
+    }  else if (vs.field.pgscan_direct_throttle > init_direct_throttle) {
+        init_direct_throttle = vs.field.pgscan_direct_throttle;
+        reclaim = DIRECT_RECLAIM_THROTTLE;
     } else if (vs.field.pgscan_kswapd > init_pgscan_kswapd) {
         init_pgscan_kswapd = vs.field.pgscan_kswapd;
         for (i = VS_PGSKIP_FIRST_ZONE; i <= VS_PGSKIP_LAST_ZONE; i++) {
@@ -3156,26 +3186,19 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
     /* Add previous cycle's decayed thrashing amount */
     thrashing += prev_thrash_growth;
 
-    /*
-     * Refresh watermarks once per min in case user updated one of the margins.
-     * TODO: b/140521024 replace this periodic update with an API for AMS to notify LMKD
-     * that zone watermarks were changed by the system software.
-     */
-    if (watermarks.high_wmark == 0 || get_time_diff_ms(&wmark_update_tm, &curr_tm) > 60000) {
-        struct zoneinfo zi;
-
-        if (zoneinfo_parse(&zi) < 0) {
-            ALOGE("Failed to parse zoneinfo!");
-            return;
-        }
-
-        calc_zone_watermarks(&zi, &watermarks);
-        wmark_update_tm = curr_tm;
+    if (zoneinfo_parse(&zi) < 0) {
+        ALOGE("Failed to parse zoneinfo!");
+        return;
     }
 
+    calc_zone_watermarks(&zi, &zone_mem_info, pgskip_deltas);
+
     /* Find out which watermark is breached if any */
-    wmark = get_lowest_watermark(&mi, &watermarks);
+    wmark = get_lowest_watermark(&mi, &zone_mem_info);
     log_meminfo(&mi, wmark);
+    if (level < VMPRESS_LEVEL_CRITICAL && (reclaim == DIRECT_RECLAIM ||
+            reclaim == DIRECT_RECLAIM_THROTTLE))
+        last_event_upgraded = true;
 
     /*
      * TODO: move this logic into a separate function
@@ -3188,7 +3211,10 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
          * free even after a kill. Mostly happens when running memory stress tests.
          */
         kill_reason = PRESSURE_AFTER_KILL;
-        strncpy(kill_desc, "min watermark is breached even after kill", sizeof(kill_desc));
+        strlcpy(kill_desc, "min watermark is breached even after kill", sizeof(kill_desc));
+    } else if (reclaim == DIRECT_RECLAIM_THROTTLE) {
+        kill_reason = DIRECT_RECL_AND_THROT;
+        strlcpy(kill_desc, "system processes are being throttled", sizeof(kill_desc));
     } else if (level >= VMPRESS_LEVEL_CRITICAL && (events != 0 || wmark <= WMARK_HIGH)) {
         /*
          * Device is too busy reclaiming memory which might lead to ANR.
@@ -3196,7 +3222,7 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
          * of the memory congestion) breaches the configured threshold.
          */
         kill_reason = CRITICAL_KILL;
-        strncpy(kill_desc, "critical pressure and device is low on memory", sizeof(kill_desc));
+        strlcpy(kill_desc, "critical pressure and device is low on memory", sizeof(kill_desc));
         if (wmark > WMARK_MIN) {
             min_score_adj = VISIBLE_APP_ADJ;
         }
@@ -3247,7 +3273,7 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
         min_score_adj = PERCEPTIBLE_APP_ADJ + 1;
     } else if (in_compaction && wmark <= WMARK_HIGH) {
         kill_reason = COMPACTION;
-        strncpy(kill_desc, "device is in compaction and low on memory", sizeof(kill_desc));
+        strlcpy(kill_desc, "device is in compaction and low on memory", sizeof(kill_desc));
         min_score_adj = VISIBLE_APP_ADJ;
     }
 
@@ -3287,8 +3313,14 @@ no_kill:
      * do not extend when kswapd reclaims because that might go on for a long time
      * without causing memory pressure
      */
-    if (events || killing || reclaim == DIRECT_RECLAIM) {
-        poll_params->update = POLLING_START;
+
+    if (events || killing || reclaim == DIRECT_RECLAIM || reclaim == DIRECT_RECLAIM_THROTTLE) {
+        if (count_upgraded_event >= psi_cont_event_thresh) {
+            poll_params->update = POLLING_CRIT_UPGRADE;
+            count_upgraded_event = 0;
+        } else if (!poll_params->poll_handler || data >= poll_params->poll_handler->data) {
+            poll_params->update = POLLING_START;
+        }
     }
 
     /* Decide the polling interval */
@@ -4032,6 +4064,10 @@ static void call_handler(struct event_handler_info* handler_info,
             s_crit_event = false;
         }
         break;
+    case POLLING_CRIT_UPGRADE:
+        poll_params->poll_start_tm = curr_tm;
+        poll_params->poll_handler = &vmpressure_hinfo[VMPRESS_LEVEL_CRITICAL];
+        break;
     }
 }
 
@@ -4197,11 +4233,12 @@ static void mainloop(void) {
             }
             if (evt->data.ptr) {
                 handler_info = (struct event_handler_info*)evt->data.ptr;
-		if (force_use_old_strategy && handler_info->handler == mp_event_common &&
-			(handler_info->data == VMPRESS_LEVEL_MEDIUM ||
-			  handler_info->data == VMPRESS_LEVEL_CRITICAL)) {
-			check_cont_lmkd_events(handler_info->data);
-		}
+                if ((handler_info->handler == mp_event_common ||
+                     handler_info->handler == mp_event_psi) &&
+                    (handler_info->data == VMPRESS_LEVEL_MEDIUM ||
+                     handler_info->data == VMPRESS_LEVEL_CRITICAL)) {
+                    check_cont_lmkd_events(handler_info->data);
+                }
                 call_handler(handler_info, &poll_params, evt->events);
             }
         }
@@ -4328,6 +4365,10 @@ static void update_perf_props() {
           strlcpy(property, perf_get_prop("ro.lmk.use_new_strategy_dup", default_value).value, PROPERTY_VALUE_MAX);
           force_use_old_strategy = (!strncmp(property,"false",PROPERTY_VALUE_MAX))? false : true;
 
+          snprintf(default_value, PROPERTY_VALUE_MAX, "%d", PSI_CONT_EVENT_THRESH);
+          strlcpy(property, perf_get_prop("ro.lmk.psi_cont_event_thresh", default_value).value,
+                  PROPERTY_VALUE_MAX);
+          psi_cont_event_thresh = strtod(property, NULL);
           /*The following properties are not intoduced by Google
            *hence kept as it is */
           strlcpy(property, perf_get_prop("ro.lmk.enhance_batch_kill", "true").value, PROPERTY_VALUE_MAX);

statslog.h
@@ -49,6 +49,7 @@ enum kill_reasons {
     DIRECT_RECL_AND_THRASHING,
     LOW_MEM_AND_SWAP_UTIL,
     COMPACTION,
+    DIRECT_RECL_AND_THROT,
     KILL_REASON_COUNT
 };
 

AOSPA/android_system_memory_lmkd

commit 36e2254da007566fecec03b7350dbe11fc72d3e5

lmkd: fix disconnect in pgskip_delta value
There's a disconnect between pgskip_delta for the zone and what
node->zones points to in calc_zone_watermarks() which leads to
incorrect watermark aggregation. Fix this by getting pgskip_delta
using zone name correspondingly instead of using indexes.

CRs-Fixed: 3224564
Fixes: 674e711fa7ed ("lmkd: Add improvements for lmkd")
Change-Id: I4687635cdd262c449b12e27206f052dd36706a9c
Signed-off-by: Sudarshan Rajagopalan <quic_sudaraja@quicinc.com>
Signed-off-by: Georgi Djakov <quic_c_gdjako@quicinc.com>

lmkd.cpp
@@ -384,6 +384,7 @@ struct zoneinfo_zone {
     union zoneinfo_zone_fields fields;
     int64_t protection[MAX_NR_ZONES];
     int64_t max_protection;
+    char name[LINE_MAX];
 };
 
 /* zoneinfo per-node fields */
@@ -1876,7 +1877,7 @@ static int zoneinfo_parse_zone(char **buf, struct zoneinfo_zone *zone) {
     return false;
 }
 
-static int zoneinfo_parse_node(char **buf, struct zoneinfo_node *node) {
+static int zoneinfo_parse_node(char **buf, struct zoneinfo_node *node, char *name) {
     int fields_to_match = ZI_NODE_FIELD_COUNT;
 
     for (char *line = strtok_r(NULL, "\n", buf); line;
@@ -1886,7 +1887,19 @@ static int zoneinfo_parse_node(char **buf, struct zoneinfo_node *node) {
         char *save_ptr;
         int64_t val;
         int field_idx;
+        char zone_name[LINE_MAX + 1];
         enum field_match_result match_res;
+
+        /*
+         * Empty zones would not have per-zone stats, so we might come across a
+         * new zone next. Update zone_name and continue parsing.
+         */
+        if (sscanf(line, "Node %d, zone %" STRINGIFY(LINE_MAX) "s", &node->id, zone_name) == 2) {
+            ALOGE("%s per-node stats not found in zone %s; moving to next zone", __func__, name);
+
+            /* update zone name to be used in struct zoneinfo_zone.name */
+            strlcpy(name, zone_name, LINE_MAX);
+        }
 
         cp = strtok_r(line, " ", &save_ptr);
         if (!cp) {
@@ -1951,7 +1964,7 @@ static int zoneinfo_parse(struct zoneinfo *zi) {
                 node = &zi->nodes[node_idx];
                 node->id = node_id;
                 zone_idx = 0;
-                if (!zoneinfo_parse_node(&save_ptr, node)) {
+                if (!zoneinfo_parse_node(&save_ptr, node, zone_name)) {
                     ALOGE("%s parse error", file_data.filename);
                     return -1;
                 }
@@ -1963,6 +1976,7 @@ static int zoneinfo_parse(struct zoneinfo *zi) {
                 ALOGE("%s parse error", file_data.filename);
                 return -1;
             }
+            strlcpy(node->zones[zone_idx].name, zone_name, LINE_MAX);
         }
     }
     if (!node) {
@@ -3174,8 +3188,25 @@ static void log_zone_watermarks(struct zoneinfo *zi,
     }
 }
 
+int64_t get_zone_pgskip_deltas_val(const char* name, int64_t *pgskip_deltas) {
+    int idx;
+
+    for (idx = 0; idx < VS_FIELD_COUNT; idx++) {
+        if (strcasestr(vmstat_field_names[idx], name) != NULL)
+            break;
+    }
+
+    if (idx == VS_FIELD_COUNT)
+        return -2;
+
+    return pgskip_deltas[PGSKIP_IDX(idx)];
+
+}
+
 void calc_zone_watermarks(struct zoneinfo *zi, struct zone_meminfo *zmi, int64_t *pgskip_deltas) {
     struct zone_watermarks *watermarks;
+    char zone_name[LINE_MAX];
+    int64_t pgskip_deltas_val = -1;
 
     memset(zmi, 0, sizeof(struct zone_meminfo));
     watermarks = &zmi->watermarks;
@@ -3193,7 +3224,15 @@ void calc_zone_watermarks(struct zoneinfo *zi, struct zone_meminfo *zmi, int64_t
                 continue;
             }
 
-            if (!pgskip_deltas[PGSKIP_IDX(i++)]) {
+            strlcpy(zone_name, zone->name, LINE_MAX);
+            pgskip_deltas_val = get_zone_pgskip_deltas_val(zone_name, pgskip_deltas);
+
+            if (pgskip_deltas_val == -2)
+                pgskip_deltas_val = pgskip_deltas[PGSKIP_IDX(i++)];
+
+            ALOGE("%s pgskip_deltas_val: %" PRId64 " pgskip_deltas[PGSKIP_IDX(i++)]: %" PRId64, __func__, pgskip_deltas_val, pgskip_deltas[PGSKIP_IDX(i++)]);
+
+            if (!pgskip_deltas_val) {
                 zmi->nr_free_pages += zone->fields.field.nr_free_pages;
                 zmi->cma_free += zone->fields.field.nr_free_cma;
                 watermarks->high_wmark += zone->max_protection + zone->fields.field.high;